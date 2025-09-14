0b86f97414747b08ff0a8800f34ed563de3c8ee2
NguyenTheLoc-chanh <22a1001d0199@students.hou.edu.com>
2025-09-14 14:25:33 +0700

    Build base backend

---
+++ backend/Khoa-Luan-Tot-Nghiep/src/test/java/com/khoaluantotnghiep/Khoa/Luan/Tot/Nghiep/KhoaLuanTotNghiepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khoaluantotnghiep.Khoa.Luan.Tot.Nghiep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhoaLuanTotNghiepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
